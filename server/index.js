7ab1ab0a35798b27da6ca24a7b3712c9d98fa4f0
Kealohi Grondin <jacob.grondin@wsu.edu>
2022-09-16 14:36:55 -0700

    refactor

--- server/index.js
+++ server/index.js
@@ -1,12 +1,11 @@
 const express = require("express");
-require("./services/passport"); //doesn't need to be assigned to anything since the file isn't returning anything
+require("./services/passport"); //doesn't need to be assigned to anything since the file isn't returning anything. Just need to attach it to this file for usage
 const authRoutes = require("./routes/authRoutes");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 /////Attach routes to express app/////
-authRoutes(app);
-//could also get rid of authroutes const and condense into:
+authRoutes(app); //could also get rid of authroutes const and condense into:
 /* require("./routes/authRoutes")(app) */
 
 /////Spin up app on PORT/////
