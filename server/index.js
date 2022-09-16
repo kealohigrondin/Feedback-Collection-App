2cde4cf16b84f5c48e8981b3109c09d6cb6dd8ff
Kealohi Grondin <jacob.grondin@wsu.edu>
2022-09-16 15:28:40 -0700

    added mongoose for mongodb atlas connection and updated readme

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
 require("./services/passport"); //doesn't need to be assigned to anything since the file isn't returning anything. Just need to attach it to this file for usage
 const authRoutes = require("./routes/authRoutes");
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+/////Connect to mongoDB via mongoose/////
+mongoose.connect(keys.mongoURI);
 
 /////Attach routes to express app/////
 authRoutes(app); //could also get rid of authroutes const and condense into:
