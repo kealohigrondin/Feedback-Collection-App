a95d4766067dceb98f021409750297118948d790
Kealohi Grondin <31868032+kealohigrondin@users.noreply.github.com>
2022-09-26 21:53:27 -0700

    Update keys.js

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,8 +1,9 @@
 //keys.js - figure out what set of credentials to use based on environment
+console.log("NODE ENVIRONMENT: " + process.env.NODE_ENV);
 if (process.env.NODE_ENV === "production") {
   //return prod set of keys
-  module.exports = require("./prod");
+  module.exports = require("./prod.js");
 } else {
   //return dev set of keys
-  module.exports = require("./dev");
+  module.exports = require("./dev.js");
 }
