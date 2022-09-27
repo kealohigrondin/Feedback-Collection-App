3de5ae6c704106dd2ab030031a6bcb15e09245af
Kealohi Grondin <jacob.grondin@wsu.edu>
2022-09-27 13:34:56 -0700

    update proxy file with new comment and update promise syntax in passport.js

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,6 +3,7 @@
 // creates a reference to the express app in a proxy within the react client
 // browser can access the express app thru the react uri + express route
 //   (i.e. localhost:3000/auth/google when express is running on localhost:5000)
+// this is for security reasons?? eliminates cross-origin requests
 
 const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
--- server/services/passport.js
+++ server/services/passport.js
@@ -24,25 +24,26 @@ const gOptions = {
   clientID: keys.googleClientID,
   clientSecret: keys.googleClientSecret,
   callbackURL: "/auth/google/callback", //tells it where to send the user back to after authing them
-  proxy: true //enables https rather than http
+  proxy: true, //enables https rather than http
 };
 
+// alternate way to call async functions that return promises instead of appending the
+//   async function call with then(res => {})
 const gStrategy = new GoogleStrategy(
   gOptions,
-  (accessToken, refreshToken, profile, done) => {
+  async (accessToken, refreshToken, profile, done) => {
     //callback function that runs after auth/google/callback gets a result from google
     //check that the user doesn't exist yet
-    User.findOne({ googleId: profile.id }).then((existingUser) => {
-      if (existingUser) {
-        console.log("USER ALREADY EXISTS");
-        done(null, existingUser); //no error, return with the existing user
-      }
-      //creates a new user and adds to mongodb
-      new User({ googleId: profile.id }).save().then((newUser) => {
-        console.log("NEW USER ADDED:\n ", newUser);
-        done(null, newUser);
-      });
-    });
+    const existingUser = await User.findOne({ googleId: profile.id });
+    if (existingUser) {
+      console.log("USER ALREADY EXISTS");
+      return done(null, existingUser); //no error, return with the existing user
+    }
+    
+    //creates a new user and adds to mongodb
+    const newUser = await new User({ googleId: profile.id }).save();
+    console.log("NEW USER ADDED:\n ", newUser);
+    done(null, newUser);
   }
 );
 passport.use(gStrategy); //tell passport to use this strategy for authentication
